2cf10dd490ee6b67e28b19f6c69ae782333700ae
Zubin Piya <zubinpiya@gmail.com>
2014-05-22 01:41:54 +1000

    Implementing Tests to fill the Methods Body
    Filling up the empty body of methods for NGramNode class and creating extra methods
    * Writing the body part of the methods
    * Implementing the failed tests to write the body for the methods
    * Testing different scenario and making it fail, and checking it to make it pass
    * Two extra methods created to support the exceptions
    * Making sure all the tests passes
    
    Close - Few more test required to check the obvious exceptions.
    No bugs.

--- ngram/NGramNode.java
+++ ngram/NGramNode.java
@@ -5,58 +5,104 @@ public class NGramNode implements NGramContainer {
 	private String context;
 	private String[] predictions;
 	private Double[] probabilities;
-	
-	public NGramNode(String context, String[] predictions, Double[] probabilities)throws NGramException {
+
+	public NGramNode(String context, String[] predictions,
+			Double[] probabilities) throws NGramException {
 		this.context = context;
 		this.predictions = predictions;
 		this.probabilities = probabilities;
 	}
-	
-	public NGramNode(String[] words, String[] predictions, Double[] probabilities)throws NGramException{
-		
-		
+
+	public NGramNode(String[] words, String[] predictions,
+			Double[] probabilities) throws NGramException {
+		if (words.length > 0) {
+			this.context = getContextPhrase(words);
+		}
+		this.predictions = predictions;
+		this.probabilities = probabilities;
+
 	}
 
 	@Override
 	public String getContext() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.context;
 	}
 
 	@Override
 	public void setContext(String context) throws NGramException {
-		// TODO Auto-generated method stub
-		
+		if (context == null || context == " ") {
+			throw new NGramException("Context is null or empty");
+		}
+		this.context = context;
+
 	}
 
 	@Override
 	public void setContext(String[] words) throws NGramException {
-		// TODO Auto-generated method stub
-		
+		if (words == null || words.length == 0) {
+			throw new NGramException("Words is empty or null");
+		}
+		this.context = getContextPhrase(words);
+
+	}
+
+	private String getContextPhrase(String[] words) {
+		if (words != null) {
+			StringBuilder sb = new StringBuilder();
+			for (String s : words) {
+				sb.append(s).append("");
+			}
+			return sb.toString();
+		} else
+			return " ";
 	}
 
 	@Override
 	public String[] getPredictions() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.predictions;
 	}
 
 	@Override
 	public void setPredictions(String[] predictions) throws NGramException {
-		// TODO Auto-generated method stub
-		
+		if (predictions == null || predictions.length == 0
+				|| emptyPrediction(predictions, " ")) {
+			throw new NGramException("Prediction not valid ");
+		}
+		this.predictions = predictions;
+
+	}
+
+	private boolean emptyPrediction(String[] predict, String empty) {
+		for (String s : predict) {
+			if (s == empty)
+				return true;
+		}
+		return false;
 	}
 
 	@Override
 	public Double[] getProbabilities() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.probabilities;
 	}
 
 	@Override
 	public void setProbabilities(Double[] probabilities) throws NGramException {
-		// TODO Auto-generated method stub
-		
+		if (probabilities == null || probabilities.length == 0 || invalidProbability(probabilities)){
+			throw new NGramException("Invalid probability");
+		}
+		this.probabilities = probabilities;
+
+	}
+
+	private boolean invalidProbability(Double[] invalidProb) {
+		for(Double d: invalidProb){
+			if(d <=0 || d > 1){
+				return true;
+			}
+		}return false;
 	}
 
 }
