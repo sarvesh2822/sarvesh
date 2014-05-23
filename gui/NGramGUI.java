a98eeb9ef5b1d92f810b7a4f97d8bb4a1e21270a
Zubin Piya <zubinpiya@gmail.com>
2014-05-23 16:08:52 +1000

    Building the GUI Body Adding some display for the gui for user's to interact. * Basic body layout is created. * All the actions are called from the main class "Suggestions" * Layout at its initial phase.
    Close: Output not satisfactory.

--- gui/NGramGUI.java
+++ gui/NGramGUI.java
@@ -1,13 +1,19 @@
 package assign2.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class NGramGUI extends JFrame implements ActionListener{
 
@@ -16,25 +22,61 @@ public class NGramGUI extends JFrame implements ActionListener{
 	 */
 	private static final long serialVersionUID = -2419228817448519511L;
 	
-	public static final int WIDTH = 600;
-	public static final int HEIGHT = 600;
+	//public static final int WIDTH = 600;
+	//public static final int HEIGHT = 600;
 	private JTextArea textArea;
+	private JButton searchButton;
 	private JButton button;
+	private JTextField textPhrase;
+	private JTextField textNumEntry;
+	
+	private JPanel topPanel;
+	private JPanel textPanel;
+	private JPanel buttomPanel;
+	
 	
 	public NGramGUI(String guiName){
 		super(guiName);
 		
 		setLayout(new BorderLayout());
 		
-		textArea = new JTextArea();
+		topPanel = new JPanel();
+		topPanel.setBackground(Color.LIGHT_GRAY);
+		
+		//GridLayout gl = new GridLayout(0,2);
+		//topPanel.setLayout(gl);
+		
+		JLabel lblEnterPhrase = new JLabel("Enter Phrase:");
+		topPanel.add(lblEnterPhrase);
+		textPhrase = new JTextField();
+		topPanel.add(textPhrase);
+		
+		JLabel lblNumEntry = new JLabel("Enter Number of output:");
+		topPanel.add(lblNumEntry);
+		textNumEntry = new JTextField();
+		topPanel.add(textNumEntry);
+		
+		searchButton = new JButton("Search");
+		topPanel.add(searchButton);
+		
+		
+		getContentPane().add(topPanel,BorderLayout.NORTH);
+		
+		
+		
+		
+		
+		
+		
+		//textArea = new JTextArea();
 		button = new JButton("Search");
 		
-		add(textArea,BorderLayout.CENTER);
+		//add(textArea,BorderLayout.CENTER);
 		add(button,BorderLayout.SOUTH);
 		
-		setSize(WIDTH, HEIGHT);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+		
+		
+		
 	}
 	
 
--- gui/Suggestions.java
+++ gui/Suggestions.java
@@ -1,17 +1,27 @@
 package assign2.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Suggestions{
 
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 500;
+	
 	
 	public static void main(String[] args) {
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				new NGramGUI("NGram Predictor");
+				NGramGUI nGramGui = new NGramGUI("NGram Predictor");
+				nGramGui.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+				nGramGui.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+				nGramGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				nGramGui.pack();
+				nGramGui.setVisible(true);
 				
 			}
 		});
